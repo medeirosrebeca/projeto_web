eea0a1061e1ed96fc0862b4be3540c4060634487
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-12-09 01:07:36 +0000

    exercicio

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Bem-vindo ao meu servidor!");
+  res.send("Bem-vindo ao meu web service!");
 });
 
 const porta: number = 3000;
